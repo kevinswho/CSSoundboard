ef1566919477e570482a6a64cce846d22933eb31
K <kevinswho@gmail.com>
2017-06-17 01:10:21 -0400

    Release 1.0

--- app/src/main/java/com/example/k/cs_soundboard/MainActivity.java
+++ app/src/main/java/com/example/k/cs_soundboard/MainActivity.java
@@ -4,7 +4,7 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.Window;
+//import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
     }
